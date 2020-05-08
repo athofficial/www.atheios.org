7c65ac3e8d2a398482c51e5dc319ae6f1d1c64b3
legacytrx <legacytrx@atheios.org>
2020-05-08 21:36:18 +0200

    Automatic pull for YouTube

---
+++ js/youtube.js
@@ -0,0 +1,38 @@
+$(function () {
+  var $content = $('#youtube-content');
+  var data = {
+    rss_url: 'https://www.youtube.com/feeds/videos.xml?channel_id=UC4UyEne_m_8ZxjjecA7yxIg'
+  };
+  $.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+    if (response.status == 'ok') {
+      var output = '';
+      $.each(response.items, function (k, item) {
+        console.log(item);
+        output += '<li class="wow fadeIn" data-wow-duration="1.2s">';
+        output += '<div class="medium-blog-post" onclick="window.open(\'' + item.guid + '\',\'mywindow\');">'
+        output += '<div class="inner_list">';
+        output += '<div class="image_wrap">';
+        output += '<img class="small" src="' +item.thumbnail+ '"alt="">';
+        output += '<a class="link_news" href="index.html"></a>';
+        output += '</div>';
+        output += '<div class="definitions_wrap">';
+        output += '<div class="title_holder">';
+        output += '<span class="medium_title">' + item.title + '</span>';
+        output += '</div>';
+        output += '<div class="date_wrap">';
+        output += '<p>' + $.format.date(item.pubDate, "MMM dd, yyyy") + '<a href="index.html">Update</a></p>';
+        output += '</div>';
+        output += '<div class="read_more">';
+        output += '<a href="'+ item.link +'"><span>Read More</span></a>';
+        output += '</div>';
+        output += '</div>';
+        output += '</div>';
+        output += '</li>';
+        //console.log(item);
+        return k < 3;
+      });
+      $content.html(output).hide();
+      $content.fadeIn('slow');
+    }
+  });
+});
